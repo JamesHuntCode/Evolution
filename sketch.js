824df345adbf6debf9744d26e073ab92e638d91f
James Hunt <jameshuntwork1997@yahoo.com>
2017-10-16 17:34:15 +0100

    Stage 2
    I have probably spend about 6 hours total now on this program. 
    
    In this version, the vehicles have been given DNA. They are now spawned either more attracted to the food, or the poison. The ones that are better at detecting food with a small perception of poison should be the vehicles that survive the longest. 
    
    Also, now they are repelled by the edge of the screen, and should always be focused around the centre. 
    
    In the next version I am looking to add the actual evolution. The vehicles will be able to pass down their DNA onto other vehicles that will receive a mutated version of that DNA which hopefully makes them slightly better than their parent. 
    
    Exciting stuff, will see how it turns out.

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
 // James Hunt - 16/10/17
 // Main JavaScript file for coding steering behaviours
 
-var vehicle;
+var vehicles = [];
 var food = [];
 var poison = [];
 
 function setup () {
-  createCanvas(700, 400);
-  vehicle = new Vehicle(width/2, height/2);
+  createCanvas(640, 360);
+  for (var i = 0; i < 20; i++) {
+    var x = random(width);
+    var y = random(height);
+    vehicles[i] = new Vehicle(x, y); // Generate 10 vehicles starting at random locations
+  }
 
-  for (var i = 0; i < 10; i++) {
+  // Add food into the world for the vehicles to eat:
+  for (var i = 0; i < 50; i++) {
     var x = random(width);
     var y = random(height);
     food.push(createVector(x, y));
   }
 
+  //Add poison into the world (hopefully) for the vehicles to avoid:
   for (var i = 0; i < 10; i++) {
     var x = random(width);
     var y = random(height);
@@ -25,22 +31,43 @@ function setup () {
 function draw () {
   background(50);
 
+  //Keep generating food into the world:
+  if (random(1) < 0.06) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  //Keep generating poison into the world:
+  if (random(1) < 0.01) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
+  }
+
+  // Draw food:
   for (var i = 0; i < food.length; i++) {
     fill(0, 255, 0);
     noStroke();
-    ellipse(food[i].x, food[i].y, 8, 8)
+    ellipse(food[i].x, food[i].y, 4, 4); // Draw the food as a green cirle
   }
 
+  // Draw poison:
   for (var i = 0; i < poison.length; i++) {
     fill(255, 0, 0);
     noStroke();
-    ellipse(poison[i].x, poison[i].y, 8, 8)
+    ellipse(poison[i].x, poison[i].y, 4, 4); // Draw the poison as a red circle
   }
 
   // Call vehicle functions to allow it to operate:
-  vehicle.eat(food);
-  vehicle.eat(poison)
-  //vehicle.seek(food);
-  vehicle.update();
-  vehicle.display();
+  for (var i = vehicles.length-1; i >= 0; i--) {
+    vehicles[i].boundaries();
+    vehicles[i].behaviours(food, poison);
+    vehicles[i].update();
+    vehicles[i].display();
+
+    if (vehicles[i].death()) {
+      vehicles.splice(i, 1); // If vehicle has died, remove it from array of vehicles
+    }
+  }
 }
--- vehicle.js
+++ vehicle.js
@@ -5,12 +5,30 @@ function Vehicle(x, y) {
   this.acceleration = createVector(0,0);
   this.velocity = createVector(0,-2);
   this.position = createVector(x,y);
-  this.r = 6;
+  this.r = 4;
   this.maxspeed = 5;
   this.maxforce = 0.2;
 
+  this.health = 1; // Will lower over time, will regen upon vehicle eating food
+
+  // This DNA will decide the level of attraction towards the food and the poison:
+  this.dna = [];
+
+  // Attraction to the food particles:
+  this.dna[0] = random(-5, 5);
+
+  // Attraction to the poison particles:
+  this.dna[1] = random(-5, 5);
+
+  // Perception of food particles:
+  this.dna[2] = random(10, 100); // Between 10 & 100 pixels
+
+  // Perception of poison particles:
+  this.dna[3] = random(10, 100); // Between 10 & 100 pixels
+
   // Method to update location of the vehicle
   this.update = function() {
+    this.health -= 0.005;
     // Update vehicle velocity
     this.velocity.add(this.acceleration);
     // Limit speed of vehicle
@@ -24,28 +42,45 @@ function Vehicle(x, y) {
     this.acceleration.add(force);
   };
 
+  // Method to keep track of how attracted to poison and food our vehicle is:
+  this.behaviours = function (good, bad) {
+    var goodAttraction = this.eat(good, 0.15, this.dna[2]);
+    var badAttraction = this.eat(bad, -0.5, this.dna[3]);
+
+    goodAttraction.mult(this.dna[0]);
+    badAttraction.mult(this.dna[1]);
+
+    this.applyForce(goodAttraction);
+    this.applyForce(badAttraction);
+  }
+
   // Method to locate and eat the closest piece of food:
-  this.eat = function (list) {
+  this.eat = function (list, nutritionalValue, perceptionRadius) {
     var record = Infinity;
     var closest = -1;
     for (var i = 0; i < list.length; i++) {
       var distance = this.position.dist(list[i]); // Get distance between vehicle location and food
-      if (distance < record) {
+      // As long as the distance is less than infinity and within the vehicle's perception, vehicle can eat that food:
+      if (distance < record && distance < perceptionRadius) {
         record = distance;
-        closest = i; //Closest is then set to the the value of i
+        closest = i;
       }
     }
 
     //If statement to check if food has actually been eaten:
     if (record < 5) {
       list.splice(closest, 1); // Remove the food that was eaten from the array of food
+      this.health += nutritionalValue; // Increase health as vehile eats food
     } else if (closest > -1) {
-      this.seek(list[closest]); // Only seek the next next bit of food if it currently exists
+      return this.seek(list[closest]); // Only seek the next next bit of food if it currently exists
     }
+
+    return createVector(0, 0); // If there is nothing for the vehicle to eat or seek, return a 0 vector
+
   };
 
   // A method that calculates a steering force towards a target
-  // STEER = DESIRED VELOCITY MINUS VELOCITY
+  // Steering force = desired velocity - current velocity
   this.seek = function(target) {
 
     var desired = p5.Vector.sub(target, this.position);  // A vector pointing from the location to the target
@@ -55,20 +90,61 @@ function Vehicle(x, y) {
 
     // Steering = Desired velocity minus velocity
     var steeringForce = p5.Vector.sub(desired, this.velocity);
-    steer.limit(this.maxforce);  // Limit to maximum steering force
+    steeringForce.limit(this.maxforce);  // Limit to maximum steering force
 
-    this.applyForce(steeringForce);
+    return steeringForce;
+  };
+
+  // Method to keep track of where a vehicle is alive or not:
+  this.death = function () {
+    return (this.health < 0); // Use boolean expression to return true or false
+  };
+
+  // Method designed to keep the vehicles away from the edges of the screen:
+  this.boundaries = function () {
+
+    var distance = 25; //Distance from the edge we want to stay
+    var desired = null;
+
+    if (this.position.x < distance) {
+      desired = createVector(this.maxspeed, this.velocity.y);
+    }
+    else if (this.position.x > width -distance) {
+      desired = createVector(-this.maxspeed, this.velocity.y);
+    }
+
+    if (this.position.y < distance) {
+      desired = createVector(this.velocity.x, this.maxspeed);
+    }
+    else if (this.position.y > height-distance) {
+      desired = createVector(this.velocity.x, -this.maxspeed);
+    }
+
+    if (desired !== null) {
+      desired.normalize();
+      desired.mult(this.maxspeed);
+      var steeringForce = p5.Vector.sub(desired, this.velocity);
+      steeringForce.limit(this.maxforce);
+      this.applyForce(steeringForce);
+    }
   };
 
   this.display = function() {
     // Draw a triangle rotated in the direction of velocity to represent our vehicle
-    var theta = this.velocity.heading() + PI/2;
-    fill(127);
-    stroke(200);
+    var angle = this.velocity.heading() + PI/2;
+
+    //Color of vehicle will change from green to red depending on their current health
+    var green = color(0, 255, 0);
+    var red = color(255, 0, 0);
+    var healthIndicator = lerpColor(red, green, this.health);
+
+    fill(healthIndicator);
+    stroke(healthIndicator);
+
     strokeWeight(1);
     push();
     translate(this.position.x,this.position.y);
-    rotate(theta);
+    rotate(angle);
     beginShape();
     vertex(0, -this.r*2);
     vertex(-this.r, this.r*2);
